f10d70f2a4f27622153b452d74fd81d144b4d617
Jovita Staskeviciene <jovita.staskeviciene@gmail.com>
2022-05-19 13:47:03 +0300

    Added delete function

--- src/components/NavBar/BookList.js
+++ src/components/NavBar/BookList.js
@@ -9,7 +9,7 @@ import '../Booklist/BookList.css';
 
 function BookList() {
 
-    const [tasks, setTasks] = useState([]); // Task State
+    const [tasks, setTasks] = useState([]);
     const [showAddTask, setShowAddTask] = useState(false); 
     
     const addTask = (task) => {
@@ -27,6 +27,20 @@ function BookList() {
       localStorage.setItem("bookAdded", JSON.stringify([...tasks, newTask]));
     }
 
+    const deleteTask = (id) => {
+      const deleteTask = tasks.filter((task) => task.id !== id);
+
+      setTasks(deleteTask);
+
+      Swal.fire({
+          icon: 'success',
+          title: 'Oops...',
+          text: 'You have successfully deleted a task!'
+      })
+
+      localStorage.setItem("bookAdded", JSON.stringify(deleteTask));
+  }
+
       useEffect(() => {
         alert("Welcome to our App");
     }, []);
@@ -40,9 +54,11 @@ function BookList() {
             
            
             {
-              tasks.length > 0 ?
-                (<Tasks tasks={tasks} />) :
-                ('No Book Found!')
+              tasks.length > 0 
+              ?
+              (<Tasks tasks={tasks} onDelete={deleteTask} />)
+              :
+              ('No Book Found!')
             }
           </div>
         </>
