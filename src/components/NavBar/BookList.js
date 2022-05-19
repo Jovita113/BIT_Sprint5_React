421fdf5164d8ca2d61b4643f0865ffe3cbd7d685
Jovita Staskeviciene <jovita.staskeviciene@gmail.com>
2022-05-19 14:17:23 +0300

    Added get function and Loading Spinner

--- src/components/NavBar/BookList.js
+++ src/components/NavBar/BookList.js
@@ -8,83 +8,114 @@ import Swal from "sweetalert2";
 import '../Booklist/BookList.css';
 
 function BookList() {
+  const [loading, setloading] = useState(true);
+  const [tasks, setTasks] = useState([]);
+  const [showAddTask, setShowAddTask] = useState(false);
 
-    const [tasks, setTasks] = useState([]);
-    const [showAddTask, setShowAddTask] = useState(false); 
-    
-    const addTask = (task) => {
-      const id = uuidv4();
-      const newTask = { id, ...task }
+  useEffect(() => {
+    setTimeout(() => {
+      setloading(false);
+    }, 3500);
+  }, [])
 
-      setTasks([...tasks, newTask]);
-
-      Swal.fire({
-          icon: 'success',
-          title: 'Yay...',
-          text: 'You have successfully added a new book!'
-      })
-
-      localStorage.setItem("bookAdded", JSON.stringify([...tasks, newTask]));
+  const getTasks = JSON.parse(localStorage.getItem("bookAdded"));
+  useEffect(() => {
+    if (getTasks == null) {
+      setTasks([])
+    } else {
+      setTasks(getTasks);
     }
+  }, [])
 
-    const deleteTask = (id) => {
-      const deleteTask = tasks.filter((task) => task.id !== id);
+  const addTask = (task) => {
+    const id = uuidv4();
+    const newTask = { id, ...task }
 
-      setTasks(deleteTask);
+    setTasks([...tasks, newTask]);
 
-      Swal.fire({
-          icon: 'success',
-          title: 'Oops...',
-          text: 'You have successfully deleted a book!'
-      })
+    Swal.fire({
+      icon: 'success',
+      title: 'Yay...',
+      text: 'You have successfully added a new book!'
+    })
 
-      localStorage.setItem("bookAdded", JSON.stringify(deleteTask));
+    localStorage.setItem("bookAdded", JSON.stringify([...tasks, newTask]));
   }
 
-    const editTask = (id) => {
-      const text = prompt("Author name");
-      const day = prompt("Book title");
-      const myData = tasks.map(x => {
-          if (x.id === id) {
-              return {
-                  ...x,
-                  text: text,
-                  day: day,
-                  id: uuidv4()
-              }
-          }
-          return x;
-      })
-      Swal.fire({
-          icon: 'success',
-          title: 'Yay...',
-          text: 'You have successfully edited an existing book!'
-      })
-      localStorage.setItem("BookAdded", JSON.stringify(myData));
-      window.location.reload();
+  const deleteTask = (id) => {
+    const deleteTask = tasks.filter((task) => task.id !== id);
+
+    setTasks(deleteTask);
+
+    Swal.fire({
+      icon: 'success',
+      title: 'Oops...',
+      text: 'You have successfully deleted a book!'
+    })
+
+    localStorage.setItem("bookAdded", JSON.stringify(deleteTask));
   }
 
-      useEffect(() => {
-        alert("Welcome to our App");
-    }, []);
-    
-    return (
-        <>
+  const editTask = (id) => {
+    const text = prompt("Author name");
+    const day = prompt("Book title");
+    const myData = tasks.map(x => {
+      if (x.id === id) {
+        return {
+          ...x,
+          text: text,
+          day: day,
+          id: uuidv4()
+        }
+      }
+      return x;
+    })
+    Swal.fire({
+      icon: 'success',
+      title: 'Yay...',
+      text: 'You have successfully edited an existing book!'
+    })
+    localStorage.setItem("BookAdded", JSON.stringify(myData));
+    window.location.reload();
+  }
+  return (
+    <>
+      {
+        loading ?
+          <div className="spinnerContainer">
+            <div className="spinner-grow text-primary" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-secondary" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-success" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-danger" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+            <div className="spinner-grow text-warning" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div> :
           <div className="container">
             <Header showForm={() => setShowAddTask(!showAddTask)} changeTextAndColor={showAddTask} />
-           
+
             {showAddTask && <AddTask onSave={addTask} />}
-            
-           
+
+            <h3>Number of Tasks: {tasks.length}</h3>
+
             {
-              tasks.length > 0 
-              ?
-              (<Tasks tasks={tasks} onDelete={deleteTask} onEdit={editTask} />)
-              :
-              ('No Book Found!')
+              tasks.length > 0
+                ?
+                (<Tasks tasks={tasks} onDelete={deleteTask} onEdit={editTask} />)
+                :
+                ('No Book Found!')
             }
           </div>
-        </>
-    )
+        }
+    </>
+  )
 }
 export default BookList;
